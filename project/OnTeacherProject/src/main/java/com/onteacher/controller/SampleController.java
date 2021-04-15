05b3e79631d7d648c0a247f130119dec3cc8825c
minami-cs <mina9301@gmail.com>
2021-04-15 14:27:43 +0900

    chore : upload project folder develop branch에 프로젝트 폴더 다시 재업로드

--- project/OnTeacherProject/src/main/java/com/onteacher/controller/SampleController.java
+++ project/OnTeacherProject/src/main/java/com/onteacher/controller/SampleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller("sampleController")
 @RequestMapping("/sample")
 public class SampleController {
-	@RequestMapping(value="/main.do", method=RequestMethod.GET)  // 주소: localhost:8090/sample/main.do
+	@RequestMapping(value="/main.do", method=RequestMethod.GET)  // 주소: localhost:8090/sample/main.do 로 들어가면 확인가능
 	public String main(Model model, HttpServletRequest request, HttpServletResponse response) {
 		model.addAttribute("page", "index");
 		return "template";
